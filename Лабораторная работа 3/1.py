e7a292af599454021aa434a0ad8ac5de3d1ba81f
sonechkq <90902767+sonechkq@users.noreply.github.com>
2024-11-24 22:17:51 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/1.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def find_item_index(items, item_to_find):
+    try:
+        index = items.index(item_to_find)  # Метод index возвращает индекс первого вхождения
+        return index
+    except ValueError:
+        return None  # Если товар не найден, возвращаем None
+
+    items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+    for find_item in ['банан', 'груша', 'персик']:
+        index_item = find_item_index(items_list, find_item)
+        if index_item is not None:
+            print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+        else:
+            print(f"Товар '{find_item}' не найден в списке.")
